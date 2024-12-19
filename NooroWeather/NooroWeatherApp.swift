b13831ef582d767466653719552fda0d33430549
nstandage <nathanstandage@gmail.com>
2024-12-19 14:39:00 -0700

    Initial Commit

---
+++ NooroWeather/NooroWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroWeatherApp.swift
+//  NooroWeather
+//
+//  Created by Nathan on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct NooroWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
